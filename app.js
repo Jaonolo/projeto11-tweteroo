0a316dc7e408b98eb5fb5e001ceebf993d468a25
João Vitor Sanches <joaovitormanolo@gmail.com>
2022-04-25 16:53:23 -0300

    feat : requisitos bonus

--- app.js
+++ app.js
@@ -12,15 +12,34 @@ app.use(express.json())
 
 app.post("/sign-up", (req, res) => {
     let {username, avatar} = req.body
+
+    if(username === undefined || avatar === undefined) {
+        res
+            .status(400)
+            .send("Todos os campos são obrigatórios!")
+        return
+    }
+
     users.push({
         username: username,
         avatar: avatar
     })
-    res.send("OK")
+    res
+        .status(201)
+        .send("OK")
 })
 
 app.post("/tweets", (req, res) => {
-    let {username, tweet} = req.body
+    let username = req.headers.user
+    let { tweet } = req.body
+    
+    if(username === undefined || tweet === undefined) {
+        res
+            .status(400)
+            .send("Todos os campos são obrigatórios!")
+        return
+    }
+
     let user = users.find(e => e.username === username)
     user = user || {avatar: ''}
     tweets.push({
@@ -28,14 +47,31 @@ app.post("/tweets", (req, res) => {
         avatar: user.avatar,
         tweet: tweet
     })
-    res.send("OK")
+    res
+        .status(201)
+        .send("OK")
 })
 
 app.get("/tweets", (req, res) => {
+    let page = req.query.page
+    if(page < 1) {
+        res
+            .status(400)
+            .send("Informe uma página válida!")
+            return
+    }
+    page = page || 1
+
     let lastTweets = []
-    for(let i = tweets.length - 1; i >= 0 && i >= tweets.length - 10; i--)
+    let pageStart = tweets.length - (page - 1)*10 - 1
+    for(let i = pageStart; i >= 0 && i > pageStart - 10; i--)
         lastTweets.push(tweets[i])
     res.send(lastTweets)
+})
+
+app.get("/tweets/:username", (req, res) => {
+    let { username } = req.params
+    res.send(tweets.filter(e => e.username === username))
 })
 
 app.get("/", (req, res) => res.send("Servidor funcionando!"))
